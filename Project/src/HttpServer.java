8727c84cb9bf040261a2a873369c4f429134d927
Cyprian Chailan <cyprian.chailan2@etu.univ-lorraine.fr>
2024-06-13 13:27:22 +0200

    azaeazea

--- Project/src/HttpServer.java
+++ Project/src/HttpServer.java
@@ -1,8 +1,5 @@
 import java.io.*;
-import java.net.InetAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.net.SocketException;
+import java.net.*;
 import java.nio.file.Files;
 import java.util.Base64;
 import java.util.HashSet;
@@ -44,73 +41,88 @@ public class HttpServer {
             Logs.logAccess("Serveur démarré sur le port " + port);
 
             while (true) {
-                try (Socket socket = serverSocket.accept()) {
-                    connectionCount++;
-                    if (!isAccepted(socket.getInetAddress())) {
-                        socket.close();
-                        connectionCount--;
-                        continue;
-                    }
-
-                    try (BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                         OutputStream out = socket.getOutputStream();
-                         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out))
-                    ) {
-                        String requestLine = reader.readLine();
-                        if (requestLine == null || requestLine.isEmpty()) {
-                            return;
-                        }
-                        Logs.logAccess(requestLine); //mettre dans un fichier /access.log
-
-                        String[] requestLineParts = requestLine.split(" ");
-                        if (requestLineParts.length < 2) {
-                            Logs.logError("Requête invalide : " + requestLine);
-                            continue;
-                        }
-
-                        String filePath = requestLineParts[1];
-                        if (filePath.equals("/status")) {
-                            sendStatusResponse(writer);
-                        } else {
-                            if (filePath.equals("/")) {
-                                filePath = "/index.html";
-                            }
-
-                            filePath = rootDirectory + filePath;
-
-                            File file = new File(filePath);
-                            if (file.exists() && !file.isDirectory()) {
-                                sendFileResponse(writer, out, file);
-                            } else {
-                                sendErrorResponse(writer, 404, "Not Found");
-                                Logs.logError("Fichier non trouvé : " + filePath);
-                            }
-                        }
-
-                    } catch (IOException e) {
-                        Logs.logError("Erreur de lecture de la requête : " + e.getMessage());
-                        if(e instanceof SocketException){
-                            Logs.logError("Information supplémentaire : " + e);
-                        }
-                    } finally {
-                        connectionCount--;
-                    }
-                } catch (IOException e) {
-                    Logs.logError("Erreur de connection client : " + e.getMessage());
-                }
+                Socket socket = serverSocket.accept();
+                new Thread(new ClientHandler(socket)).start();
             }
         } catch (IOException e) {
             Logs.logError("Erreur de connection serveur : " + e.getMessage());
         }
     }
 
-    private static void sendFileResponse(BufferedWriter writer, OutputStream out, File file) throws IOException {
-        String mimeType = getMimeType(file);
-        byte[] fileData = Files.readAllBytes(file.toPath());
+    private static class ClientHandler implements Runnable {
+        private Socket socket;
 
-        if (mimeType.startsWith("image/") || mimeType.startsWith("video/") || mimeType.startsWith("audio/")) {
-            fileData = Base64.getEncoder().encode(fileData);
+        public ClientHandler(Socket socket) {
+            this.socket = socket;
         }
+
+        @Override
+        public void run() {
+            connectionCount++;
+            try {
+                if (!isAccepted(socket.getInetAddress())) {
+                    socket.close();
+                    connectionCount--;
+                    return;
+                }
+
+                try (BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                     OutputStream out = socket.getOutputStream();
+                     BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out))
+                ) {
+                    String requestLine = reader.readLine();
+                    if (requestLine == null || requestLine.isEmpty()) {
+                        Logs.logError("Requête vide ou nulle reçue");
+                        return;
+                    }
+                    Logs.logAccess(requestLine);
+
+                    String[] requestLineParts = requestLine.split(" ");
+                    if (requestLineParts.length < 2) {
+                        Logs.logError("Requête invalide : " + requestLine);
+                        return;
+                    }
+
+                    String filePath = requestLineParts[1];
+                    if (filePath.equals("/status")) {
+                        sendStatusResponse(writer);
+                    } else {
+                        if (filePath.equals("/")) {
+                            filePath = "/index.html";
+                        }
+
+                        filePath = rootDirectory + filePath;
+
+                        File file = new File(filePath);
+                        if (file.exists() && !file.isDirectory()) {
+                            sendFileResponse(writer, out, file);
+                        } else {
+                            sendErrorResponse(writer, 404, "Not Found");
+                            Logs.logError("Fichier non trouvé : " + filePath);
+                        }
+                    }
+                } catch (IOException e) {
+                    Logs.logError("Erreur de lecture de la requête : " + e.getMessage());
+                    if (e instanceof SocketException) {
+                        Logs.logError("SocketException : " + e.getMessage());
+                    }
+                } finally {
+                    connectionCount--;
+                }
+            } catch (IOException e) {
+                Logs.logError("Erreur de connection client : " + e.getMessage());
+            }
+        }
+    }
+
+    private static void sendFileResponse(BufferedWriter writer, OutputStream out, File file) throws IOException {
+        String mimeType = Files.probeContentType(file.toPath());
+        System.out.println(mimeType);
+        if (mimeType == null) {
+            mimeType = "application/octet-stream";
+        }
+
+        byte[] fileData = Files.readAllBytes(file.toPath());
 
         writer.write("HTTP/1.1 200 OK\r\n");
         writer.write("Content-Type: " + mimeType + "\r\n");
@@ -120,37 +132,6 @@ public class HttpServer {
 
         out.write(fileData);
         out.flush();
-    }
-
-    private static String getMimeType(File file) {
-        String extension = "";
-
-        String fileName = file.getName();
-        int i = fileName.lastIndexOf('.');
-        if (i > 0) {
-            extension = fileName.substring(i + 1);
-        }
-
-        switch (extension) {
-            case "html":
-                return "text/html";
-            case "css":
-                return "text/css";
-            case "jpg":
-            case "jpeg":
-                return "image/jpeg";
-            case "png":
-                return "image/png";
-            case "gif":
-                return "image/gif";
-            case "mp4":
-                return "video/mp4";
-            case "mp3":
-                return "audio/mpeg";
-            // Add more cases as needed
-            default:
-                return "application/octet-stream";
-        }
     }
 
     private static void sendErrorResponse(BufferedWriter writer, int statusCode, String statusMessage) throws IOException {
